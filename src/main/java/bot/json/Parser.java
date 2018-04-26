65b2a12ac1ed89404197173a2b741189c8897080
Kirill Bertosh <kirillbertosh@gmail.com>
2018-04-26 03:24:57 +0300

    added search ащк zero currency pairs

--- src/main/java/bot/json/Parser.java
+++ src/main/java/bot/json/Parser.java
@@ -2,9 +2,12 @@ package bot.json;
 
 import bot.entities.Info;
 import bot.entities.Ticker;
+import bot.filemanager.FileManager;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.util.*;
 
 public class Parser {
 
@@ -29,24 +32,64 @@ public class Parser {
         return resultInfo;
     }
 
-    public Ticker parseTicker(String ticker, String currencyPair) {
+    public List<Ticker> parseAllTickersFromResponseFiles(Info info) {
+        List<Ticker> tickerList = new ArrayList<Ticker>();
+        int counter = 0;
+        for (int i = 0; i < 164; i++) {
+            String tickerResponse = FileManager.loadFromFile("tickers" + i + ".txt");
+            for (int j = 0; j < 43; j++) {
+                try {
+                    JSONObject jsonObject = (JSONObject) jsonParser.parse(tickerResponse);
+                    JSONObject structure = (JSONObject) jsonObject.get(info.getPairs()[counter]);
+                    Ticker ticker = parseTicker(structure, info.getPairs()[counter]);
+                    tickerList.add(ticker);
+                    System.out.println(counter);
+                    counter++;
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return tickerList;
+    }
+
+    public List<Ticker> parseAllTickers(String tickers, Info info) {
         try {
-            Ticker resultTicker = new Ticker();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(ticker);
-            JSONObject structure = (JSONObject) jsonObject.get(currencyPair);
-            resultTicker.setHigherPrice((Double) structure.get("high"));
-            resultTicker.setLowerPrice((Double) structure.get("low"));
-            resultTicker.setAveragePrice((Double) structure.get("avg"));
-            resultTicker.setVolume((Double) structure.get("vol"));
-            resultTicker.setVolumeInCurrency((Double) structure.get("vol_cur"));
-            resultTicker.setLastDealPrice((Double) structure.get("last"));
-            resultTicker.setBuyPrice((Double) structure.get("buy"));
-            resultTicker.setSellPrice((Double) structure.get("sell"));
-            resultTicker.setUpdated((Long) structure.get("updated"));
-            return resultTicker;
+            List<Ticker> tickerList = new ArrayList<Ticker>();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(tickers);
+            JSONObject structure = (JSONObject) jsonObject.get(info.getPairs()[0]);
+            System.out.println(structure);
+            return tickerList;
         } catch (ParseException e) {
             e.printStackTrace();
             return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
+    }
+
+    public Ticker parseTicker(JSONObject structure, String currencyPair) {
+        Ticker resultTicker = new Ticker();
+        resultTicker.setTickerPairName(currencyPair);
+        resultTicker.setHigherPrice(
+                structure.get("high").equals(0L) ? 0 : new Double(structure.get("high").toString()));
+        resultTicker.setLowerPrice(
+                structure.get("low").equals(0L) ? 0 : new Double(structure.get("low").toString()));
+        resultTicker.setAveragePrice(
+                structure.get("avg").equals(0L) ? 0 : new Double(structure.get("avg").toString()));
+        resultTicker.setVolume(
+                structure.get("vol").equals(0L) ? 0 : new Double(structure.get("vol").toString()));
+        resultTicker.setVolumeInCurrency(
+                structure.get("vol_cur").equals(0L) ? 0 : new Double(structure.get("vol_cur").toString()));
+        resultTicker.setLastDealPrice(
+                structure.get("last").equals(0L) ? 0 : new Double(structure.get("last").toString()));
+        resultTicker.setBuyPrice(
+                structure.get("buy").equals(0L) ? 0 : new Double(structure.get("buy").toString()));
+        resultTicker.setSellPrice(
+                structure.get("sell").equals(0L) ? 0 : new Double(structure.get("sell").toString()));
+        resultTicker.setUpdated((Long) structure.get("updated"));
+        System.out.println(resultTicker.toString());
+        return resultTicker;
     }
 }
